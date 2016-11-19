5b3d6bc1797f80c215bb7255f2fbc7e987b5c4f4
tddome <troydome@yahoo.com>
2016-11-18 22:20:50 -0800

    Minor cleanup in Product

--- product.go
+++ product.go
@@ -28,27 +28,27 @@ func ProductDelete(p []*Product, i int) []*Product {
 	return append(p[:i], p[i+1:]...)
 }
 
-//ProductUpdatePrice - Update price from a Product using index
-func ProductUpdatePrice(p []*Product, i int, v float32) []*Product {
-	p[i].pPrice = v
-	return p
-}
-
-//ProductUpdateName - Update name from a Product using index
+//ProductUpdateName - Update Name from a Product using index
 func ProductUpdateName(p []*Product, i int, n string) []*Product {
 	p[i].pName = n
 	return p
 }
 
-//ProductUpdateDesc - Update desc from a Product using index
+//ProductUpdateID - Update ID from a Product using index
 func ProductUpdateID(p []*Product, i int, id int) []*Product {
 	p[i].pID = id
 	return p
 }
 
-//ProductUpdateType - Update desc from a Product using index
+//ProductUpdateType - Update Type from a Product using index
 func ProductUpdateType(p []*Product, i int, t string) []*Product {
 	p[i].pType = t
+	return p
+}
+
+//ProductUpdatePrice - Update Price from a Product using index
+func ProductUpdatePrice(p []*Product, i int, v float32) []*Product {
+	p[i].pPrice = v
 	return p
 }
 
