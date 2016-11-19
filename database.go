1304af855332f593be46a929229b60f7e81c84f3
alfalfadil <shaylyn.wetts@gmail.com>
2016-11-19 14:46:04 -0800

    Revert "updates to database and user; others"
    This reverts commit 916f8c8d4be91db134e2bd75459bdc77e9308431.

--- database.go
+++ database.go
@@ -9,11 +9,6 @@ func AddUserToDatabase(u user) {
 	userList = append(userList, u)
 }
 
-//ProductDelete - Delete a Product from the Product list using index
-//func ProductDelete(p []product, i int) []product {
-//	return append(p[:i], p[i+1:]...)
-//}
-
 func ReturnUserListLength() int {
 	return len(userList)
 }
@@ -21,11 +16,6 @@ func ReturnUserListLength() int {
 func AddProductToDatabase(p product) {
 	productList = append(productList, p)
 }
-
-//ProductDelete - Delete a Product from the Product list using index
-//func ProductDelete(p []product, i int) []product {
-//	return append(p[:i], p[i+1:]...)
-//}
 
 func ReturnProductListLength() int {
 	return len(productList)
